485024496147e1d2abb250dbf8a3fc8c71ed2cc9
Rizki Agus <rizkiagussalim@apps.ipb.ac.id>
2023-09-17 17:57:41 +0700

    Praktik array, object, perulangan, dan fungsi

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 import moment from "moment";
+
 const now = moment();
-console.log(now);
+
+// Praktek array
+const users = ["Rizki Agus", "Muhammad Rizal", "Agus Salim"];
+
+console.log(users);
+
+// Praktek object
+const product = {
+  name: "iPhone 14 Pro",
+  variant: "512 GB",
+  price: 26900000,
+  available: true,
+};
+
+console.log(product);
+
+// Praktek perulangan
+for (const user of users) {
+  console.log(user);
+}
+
+// Praktek fungsi
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(12, 8));
